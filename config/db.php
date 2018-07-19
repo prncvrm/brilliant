b663676fbfa7840522db62a74c9f46f7b8d8d6e4
Prince Verma <prncvrm@gmail.com>
2018-07-19 16:05:55 +0530

    db

--- config/db.php
+++ config/db.php
@@ -1,16 +1,22 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
+    /*'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=say2neeraj.com;dbname=say2neer_test_gvr',
     'username' => 'say2neer_endeavo',
     'password' => 'root123',
-    'charset' => 'utf8',
+    'charset' => 'utf8',*/
     /*'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=gvr',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',*/
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=gvr.cvczowsc1hmw.ap-south-1.rds.amazonaws.com:3306;dbname=gvr',
+    'username' => 'root',
+    'password' => 'password',
+    'charset' => 'utf8',
+
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
